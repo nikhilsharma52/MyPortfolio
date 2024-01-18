65e83d3aefaa3f799e5e7f9d17e3c2fbb3b3f37e
Nikhil Sharma <21BCS1701@cuchd.in>
2024-01-19 00:51:43 +0530

    home page making 3.1(title size fixed)

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
     return (
         <div className="home__data">
-            <h1 className="home__title">Nikhil Shar
+            <h1 className="home__title">Nikhil Sharma
                 <svg
                     width="36"
                     height="36"
